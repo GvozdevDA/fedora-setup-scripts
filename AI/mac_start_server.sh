753026a2985c1b839f01752053c86b2bfc1f170b
Dmitrii Gvozdev <dmitrii@gvozdev.dev>
2026-07-13 02:34:00 +0200

    Removed odysseus from hot start

--- AI/mac_start_server.sh
+++ AI/mac_start_server.sh
@@ -1,15 +1,16 @@
 # Start odysseus
-cd ~/Documents/Projects/odysseus
-git checkout main
-git pull
-docker compose up -d --build  & # to start odysseus
-tailscale serve --http=7000 7000 & #to expose odysseus
+# cd ~/Documents/Projects/odysseus
+# git checkout main
+# git pull
+# docker compose up -d --build  & # to start odysseus
+# tailscale serve --http=7000 7000 & #to expose odysseus
 
 # Start llama.cpp server
 llama-server -ngl 99 -fa on -ub 2048 -b 2048 --cache-type-k q8_0 --cache-type-v q8_0 --port 7001 --no-webui -hf unsloth/gpt-oss-20b-GGUF &
 tailscale serve --http=7001 7001 &
 
 # Start Hermes
+hermes gateway
 hermes dashboard --host 0.0.0.0 --port 7002 &
 tailscale serve --http=7003 7002 &
 
